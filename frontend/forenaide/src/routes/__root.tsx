13856da116976a46fe044e1a703f0fc2da819fa4
seanl01 <57535001+seanl01@users.noreply.github.com>
2025-01-15 15:39:11 +0800

    build: setup frontend tanstack router and shadcn

---
+++ frontend/forenaide/src/routes/__root.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import "../index.css"
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <>
+      <div className="p-2 flex gap-2 text-lg">
+        <Link
+          to="/"
+          activeProps={{
+            className: 'font-bold',
+          }}
+          activeOptions={{ exact: true }}
+        >
+          Home
+        </Link>{' '}
+        <Link
+          to="/about"
+          activeProps={{
+            className: 'font-bold',
+          }}
+        >
+          About
+        </Link>
+        <Link
+          to="/upload"
+          activeProps={{
+            className: 'font-bold',
+          }}
+        >
+          Upload
+        </Link>
+
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  )
+}
