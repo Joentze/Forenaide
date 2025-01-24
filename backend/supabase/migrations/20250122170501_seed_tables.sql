9bb1a07f5373d9381f2450c9703a381cc2a9be7f
Pixalesce <pixalesce@gmail.com>
2025-01-24 10:18:32 +0800

    feat: updated configuration to adopt proper utilisation of migration scripts

---
+++ backend/supabase/migrations/20250122170501_seed_tables.sql
@@ -0,0 +1,47 @@
+create table templates (
+    id uuid primary key,
+    name varchar(100) not null,
+    description text,
+    schema json not null,
+    created_at timestamp default current_timestamp not null
+);
+
+create table strategies (
+    id uuid primary key,
+    strategy varchar(255) not null,
+    name varchar(100) not null,
+    description text
+);
+
+create table pipeline_runs (
+    id uuid primary key,
+    name varchar(100) not null,
+    description text,
+    strategy_id uuid not null,
+    schema json not null,
+    status varchar(255) not null,
+    started_at timestamp default current_timestamp not null,
+    completed_at timestamp default current_timestamp not null,
+    foreign key (strategy_id) references strategies(id)
+);
+
+create table data_sources (
+    id uuid primary key,
+    uri varchar(255) not null,
+    format varchar(50) not null
+);
+
+create table sources_pipeline (
+    id uuid primary key,
+    pipeline_id uuid not null,
+    source_id uuid not null,
+    foreign key (pipeline_id) references pipeline_runs(id),
+    foreign key (source_id) references data_sources(id)
+);
+
+create table outputs (
+    id uuid primary key,
+    pipeline_id uuid not null,
+    uri varchar(255) not null,
+    foreign key (pipeline_id) references pipeline_runs(id)
+);
