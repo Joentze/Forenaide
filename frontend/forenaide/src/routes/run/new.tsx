22ff96ddfcb56a91168aff5b02dc056c950399cc
Wx <26271681+JamesCrr@users.noreply.github.com>
2025-02-08 16:03:21 +0800

    Template selection

--- frontend/forenaide/src/routes/run/new.tsx
+++ frontend/forenaide/src/routes/run/new.tsx
@@ -2,8 +2,10 @@ import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Combobox } from "@/components/ui/combobox";
+import { useCombobox } from "@/hooks/useCombobox";
 import { useDropzone } from 'react-dropzone'
-import { File, Settings, CheckCircle } from 'lucide-react'
+import { File as LRFile, Settings, CheckCircle } from 'lucide-react'
 
 export const Route = createFileRoute('/run/new')({
   component: PipelineComponent,
@@ -124,6 +126,11 @@ function FileUpload({ files, setFiles }: FileUploadProps) {
   )
 }
 
+
+const templatesPlaceholders = [
+  { value: "template1", label: "template1" },
+  { value: "template2", label: "template2" },
+];
 function ConfigUpload({
   configFile,
   setConfigFile,
@@ -145,6 +152,11 @@ function ConfigUpload({
     maxFiles: 1, // Restrict to only one file
   })
 
+  const combobox = useCombobox("", (value: string) => {
+    if (value == "") return
+    setConfigFile(new File(["content"], value, ));
+  });
+
   return (
     <div>
       <h3 className="mb-4 text-lg font-bold">Upload your configuration file</h3>
@@ -158,40 +170,42 @@ function ConfigUpload({
         </p>
       </div>
 
-      {configFile && (
-        <div className="mt-4">
-          <h4 className="font-semibold mb-2">Selected Configuration File:</h4>
-          <div className="bg-gray-100 p-2 rounded flex justify-between items-center">
-            <span>
-              {configFile.name} ({(configFile.size / 1024).toFixed(2)} KB)
-            </span>
-            <Button
-              size="sm"
-              variant="destructive"
-              onClick={() => setConfigFile(null)}
-            >
-              Remove
-            </Button>
-          </div>
-        </div>
-      )}
-
       <h4 className="font-semibold mt-4 mb-2">
         Or select a previously uploaded configuration file:
       </h4>
-      <ul className="space-y-2">
-        {previousConfigs.map((config, index) => (
-          <li
-            key={index}
-            className="flex justify-between items-center bg-gray-100 p-2 rounded cursor-pointer"
-            onClick={() => setConfigFile(new File([], config.name))}
-          >
-            <span>
-              {config.name} ({(config.size / 1024).toFixed(2)} KB)
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <Combobox options={templatesPlaceholders} comboboxState={combobox} />
+        {/* <p className="mt-4">Selected: {selectedFramework || "None"}</p> */}
+      </div>
+
+      {console.log(configFile)}
+      {configFile && (<Card className="card">
+        <CardHeader className="card-header">
+          <CardTitle className="card-title">
+            Current Template
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="card-content">
+          <div className="mt-4">
+            <h4 className="font-semibold mb-2">Selected Configuration File:</h4>
+            <div className="bg-gray-100 p-2 rounded flex justify-between items-center">
+              <span>
+                {configFile.name} ({(configFile.size / 1024).toFixed(2)} KB)
+              </span>
+              <Button
+                size="sm"
+                variant="destructive"
+                onClick={() => {
+                  combobox.reset()
+                  setConfigFile((prev) => null)
+                }}
+              >
+                Remove
+              </Button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>)}
     </div>
   )
 }
@@ -240,7 +254,7 @@ function PipelineComponent() {
     {
       label: 'File Upload',
       content: <FileUpload files={files} setFiles={setFiles} />,
-      icon: <File />,
+      icon: <LRFile />,
     },
     {
       label: 'Configuration',
